f5e114e60fe9252988ae7395ddab4b9edd8f9580
MingtaoWANG0 <mingtao.wang@clearexpress.fr>
2024-04-11 23:12:22 +0200

    test 01

---
+++ test01.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: Mingtao WANG
+ * @Date: 2024/4/11
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
