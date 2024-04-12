61cfb828e6677dbf607ba85727a3a580c66f49eb
MingtaoWANG0 <mingtao.wang@clearexpress.fr>
2024-04-12 09:37:54 +0200

    test 01

--- test01.java
+++ test01.java
@@ -1,3 +1,6 @@
+import java.math.BigDecimal;
+import java.util.HashMap;
+
 /**
  * @Author: Mingtao WANG
  * @Date: 2024/4/11
@@ -5,6 +8,15 @@
 public class test01 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 
+    public void testEmailDomainTransportOrder() {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+        System.out.println("Hello World!");
+
+    }
 }
